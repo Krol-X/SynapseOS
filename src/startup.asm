e0c55cd425b1ebec15cf10a5583e9706503f234a
0Nera <71536443+0Nera@users.noreply.github.com>
2021-07-22 13:21:10 +0300

    Changing code readability

--- src/startup.asm
+++ src/startup.asm
@@ -1,23 +1,27 @@
 format ELF
 public _start
 extrn kernel_main
+
 section ".text" executable
+
 _start:
-movzx edx, dl
-push edx
-push esi
-push ebx
-lgdt [gdtr]
-call kernel_main
+    movzx edx, dl
+    push edx
+    push esi
+    push ebx
+    lgdt [gdtr]
+    call kernel_main
 @@:
-;cli
-;hlt
-jmp @b
+    ;cli
+    ;hlt
+    jmp @b
+
 section ".data" writable
+
 gdt:
-dq 0
-dq 0x00CF9A000000FFFF
-dq 0x00CF92000000FFFF
-gdtr:
-dw $ - gdt
-dd gdt
+    dq 0
+    dq 0x00CF9A000000FFFF
+    dq 0x00CF92000000FFFF
+    gdtr:
+    dw $ - gdt
+    dd gdt
