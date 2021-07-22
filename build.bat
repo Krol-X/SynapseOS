eb652925f9b6d9ce1bdd321ad7987eb5476e16d9
0Nera <71536443+0Nera@users.noreply.github.com>
2021-07-22 13:17:01 +0300

    Global structure change

---
+++ build.bat
@@ -0,0 +1,13 @@
+@Echo off
+::set make_listfs = "utils/make_listfs.exe"
+::set dd = "utils/dd.exe"
+::set fasm = "utils/FASM.EXE"
+
+echo Fasm utils/FASM.EXE
+start utils/FASM.EXE src/boot.asm bin/boot.bin
+start utils/dd.exe if=bin/boot.bios.bin of=bin/boot_sector.bin bs=512 count=1
+start utils/dd.exe if=bin/boot.bios.bin of=disk/boot.bin bs=1 skip=512
+start utils/make_listfs.exe of=disk.img bs=512 size=2880 boot=bin/boot_sector.bin src=./disk
+
+
+pause
