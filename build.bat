a022551e8f939b5683b72238397ad7669b0445be
0Nera <71536443+0Nera@users.noreply.github.com>
2021-07-22 15:21:32 +0300

    Move disk.img to bin folder

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ echo Fasm utils/FASM.EXE
 start utils/FASM.EXE src/boot.asm bin/boot.bin
 start utils/dd.exe if=bin/boot.bios.bin of=bin/boot_sector.bin bs=512 count=1
 start utils/dd.exe if=bin/boot.bios.bin of=disk/boot.bin bs=1 skip=512
-start utils/make_listfs.exe of=disk.img bs=512 size=2880 boot=bin/boot_sector.bin src=./disk
+start utils/make_listfs.exe of=bin/disk.img bs=512 size=2880 boot=bin/boot_sector.bin src=./disk
 
 
 pause
