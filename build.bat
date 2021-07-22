169332e2652c0910fab7cc6249ee40bfb01a2590
0Nera <71536443+0Nera@users.noreply.github.com>
2021-07-22 15:29:33 +0300

    Add Kernel compilation

--- build.bat
+++ build.bat
@@ -1,10 +1,21 @@
 @Echo off
 
-echo Fasm utils/FASM.EXE
+echo boot.bin
 start utils/FASM.EXE src/boot.asm bin/boot.bin
-start utils/dd.exe if=bin/boot.bios.bin of=bin/boot_sector.bin bs=512 count=1
-start utils/dd.exe if=bin/boot.bios.bin of=disk/boot.bin bs=1 skip=512
-start utils/make_listfs.exe of=bin/disk.img bs=512 size=2880 boot=bin/boot_sector.bin src=./disk
 
+
+echo startup.o
+start utils/FASM.EXE src/startup.asm bin/startup.o
+
+echo main.o
+gcc -c -m32 -ffreestanding -o bin/main.o src/main.c
+
+echo link 
+ld --oformat=binary -melf_i386 -T script.ld -o kernel.bin startup.o main.o
+
+echo Make disk.img
+start utils/dd.exe if=bin/boot.bin of=bin/boot_sector.bin bs=512 count=1
+start utils/dd.exe if=bin/boot.bin of=disk/boot.bin bs=1 skip=512
+start utils/make_listfs.exe of=bin/disk.img bs=512 size=2880 boot=bin/boot_sector.bin src=./disk
 
 pause
