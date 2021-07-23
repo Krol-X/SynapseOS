22ddbf1b6a71613af944d8ea4843d4fb2270a074
0Nera <71536443+0Nera@users.noreply.github.com>
2021-07-23 15:18:18 +0300

    More nice kernel

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,12 +1,14 @@
 #include "../lib/stdlib.h"
+#include "../lib/tty.h"
+
 typedef struct {
     uint64 base;
     uint64 size;
 } BootModuleInfo;
 
 void kernel_main(uint8 boot_disk_id, void *memory_map, BootModuleInfo *boot_module_list) {
-    char *screen_buffer = (void*)0xB8000;
-    memset_word(screen_buffer, 0x0E00, 2000);
-    char msg[] = {'H',0x0E,'e',0x0E,'l',0x0E,'l',0x0E,'o',0x0E,' ',0x0E,'w',0x0E,'o',0x0E,'r',0x0E,'l',0x0E,'d',0x0E,'!',0x0E};
-    memcpy(screen_buffer, msg, sizeof(msg));
+    init_tty();
+    set_text_attr(0x1F);
+    clear_screen();
+    out_string("***SynapseOS 0001***\n\nEnter help to get information about commands\n");
 }
