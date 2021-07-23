3e1ca97891d32c79741f2a8d42a3269da12f3340
0Nera <71536443+0Nera@users.noreply.github.com>
2021-07-23 15:18:29 +0300

    Build fix with new libs

--- build.bat
+++ build.bat
@@ -1,12 +1,34 @@
 @Echo off
 
+echo build SynapseOS 0001
+echo *************************
+
 ::Build bootloader
+echo build bootloader
+
 echo boot.bin
 start utils/FASM.EXE src/boot/boot.asm bin/boot.bin
 
+
+
+echo *************************
 ::Build libs
+echo build libs
+
+
+::Build Stdlib
 echo stdlib.o
 i686-elf-gcc -c -m32 -ffreestanding -o bin/stdlib.o src/lib/stdlib.c
+
+::Build Text Driver
+echo tty.o
+i686-elf-gcc -c -m32 -ffreestanding -o bin/tty.o src/lib/tty.c
+
+
+
+echo *************************
+::Build kernel
+echo build Kernel
 
 ::Build Kernel starter
 echo startup.o
@@ -18,8 +40,11 @@ i686-elf-gcc -c -m32 -ffreestanding -o bin/main.o src/kernel/main.c
 
 ::Link kernel with starter and libs
 echo link 
-i686-elf-ld -T src/script.ld -o bin/kernel.bin bin/startup.o bin/stdlib.o bin/main.o --entry _start
+i686-elf-ld -T src/script.ld -o bin/kernel.bin bin/startup.o bin/stdlib.o bin/tty.o bin/main.o --entry _start
 
+
+
+echo *************************
 ::Make img file
 echo Make disk.img
 start utils/dd.exe if=bin/boot.bin of=bin/boot_sector.bin bs=512 count=1
@@ -27,4 +52,6 @@ start utils/dd.exe if=bin/boot.bin of=disk/boot.bin bs=1 skip=512
 cp bin/kernel.bin disk/kernel.bin
 start utils/make_listfs.exe of=disk.img bs=512 size=2880 boot=bin/boot_sector.bin src=./disk 
 
+echo *************************
+echo Done
 pause
