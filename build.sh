1610d46a9897f18540676e3b840eba7bbacb7639
0Nera <71536443+0Nera@users.noreply.github.com>
2021-07-23 16:41:55 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,26 @@
+echo build SynapseOS 0001
+echo *************************
+echo build bootloader
 echo boot.bin
-FASM src/boot.asm bin/boot.bin
+./utils/FASM src/boot/boot.asm bin/boot.bin
+echo *************************
+echo build libs
+echo stdlib.o
+./utils/i686-elf-gcc -c -m32 -ffreestanding -o bin/stdlib.o src/lib/stdlib.c
+echo tty.o
+./utils/i686-elf-gcc -c -m32 -ffreestanding -o bin/tty.o src/lib/tty.c
+echo *************************
+echo build Kernel
 echo startup.o
-FASM src/startup.asm bin/startup.o
+./utils/FASM src/kernel/startup.asm bin/startup.o
 echo main.o
-gcc -c -m32 -ffreestanding -o bin/main.o src/main.c
+./utils/i686-elf-gcc -c -m32 -ffreestanding -o bin/main.o src/kernel/main.c
 echo link 
-ld --oformat=binary -melf_i386 -T src/script.ld -o kernel.bin bin/startup.o bin/main.o
+./utils/i686-elf-ld -T src/script.ld -o bin/kernel.bin bin/startup.o bin/stdlib.o bin/tty.o bin/main.o --entry _start
+echo *************************
 echo Make disk.img
 dd if=bin/boot.bin of=bin/boot_sector.bin bs=512 count=1
 dd if=bin/boot.bin of=disk/boot.bin bs=1 skip=512
-./utils/make_listfs of=bin/disk.img bs=512 size=2880 boot=bin/boot_sector.bin src=./disk
+cp bin/kernel.bin disk/kernel.bin
+./utils/make_listfs of=disk.img bs=512 size=2880 boot=bin/boot_sector.bin src=./disk 
+echo Done
