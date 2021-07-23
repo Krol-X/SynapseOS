3a14358869cf846132f17e49fcd6d2238690a474
0Nera <71536443+0Nera@users.noreply.github.com>
2021-07-23 16:58:38 +0300

    Add interrupts

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,5 +1,6 @@
 #include "../lib/stdlib.h"
 #include "../lib/tty.h"
+#include "../lib/interrupts.h"
 
 typedef struct {
     uint64 base;
@@ -7,9 +8,12 @@ typedef struct {
 } BootModuleInfo;
 
 void kernel_main(uint8 boot_disk_id, void *memory_map, BootModuleInfo *boot_module_list) {
+    init_interrupts();
     init_tty();
+    
     set_text_attr(0x1F);
     clear_screen();
+
     out_string("***SynapseOS 0001***\n\n");
     printf("Boot disk id is %d\n", boot_disk_id);
     printf("Memory map at 0x%x\n", memory_map);
@@ -17,4 +21,5 @@ void kernel_main(uint8 boot_disk_id, void *memory_map, BootModuleInfo *boot_modu
     printf("String is %s, char is %c, number is %d, hex number is 0x%x\n\n",
         __DATE__, 'A', 1234, 0x1234);
     printf("Enter help to get information about commands\n");
+
 }
