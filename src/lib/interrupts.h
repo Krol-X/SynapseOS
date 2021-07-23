6debe7f39b6f85968e838b2acdc22677c61f38cb
0Nera <71536443+0Nera@users.noreply.github.com>
2021-07-23 17:05:56 +0300

    Fix bug

--- src/lib/interrupts.h
+++ src/lib/interrupts.h
@@ -6,8 +6,8 @@ uint8 irq_count;
 
 
 #define IRQ_HANDLER(name) void name(); \
-asm(#name ": pusha \n call _" #name " \n movb $0x20, %al \n outb %al, $0x20 \n outb %al, $0xA0 \n popa \n iret"); \
-void _ ## name()
+    asm(#name ": pusha \n call _" #name " \n movb $0x20, %al \n outb %al, $0x20 \n outb %al, $0xA0 \n popa \n iret"); \
+        void _ ## name()
 
 
 void init_interrupts();
