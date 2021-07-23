0068c7d923e7f3bbbd3dcf4f5190cb07fb5b0a00
0Nera <71536443+0Nera@users.noreply.github.com>
2021-07-23 14:01:45 +0300

    Create build with stdlib

--- build.bat
+++ build.bat
@@ -1,18 +1,26 @@
 @Echo off
 
+::Build bootloader
 echo boot.bin
 start utils/FASM.EXE src/boot/boot.asm bin/boot.bin
 
+::Build libs
+echo stdlib.o
+i686-elf-gcc -c -m32 -ffreestanding -o bin/stdlib.o src/lib/stdlib.c
 
+::Build Kernel starter
 echo startup.o
 start utils/FASM.EXE src/kernel/startup.asm bin/startup.o
 
+::Build Kernel
 echo main.o
 i686-elf-gcc -c -m32 -ffreestanding -o bin/main.o src/kernel/main.c
 
+::Link kernel with starter and libs
 echo link 
-i686-elf-ld -T src/script.ld -o bin/kernel.bin bin/startup.o bin/main.o --entry _start
+i686-elf-ld -T src/script.ld -o bin/kernel.bin bin/startup.o bin/stdlib.o bin/main.o --entry _start
 
+::Make img file
 echo Make disk.img
 start utils/dd.exe if=bin/boot.bin of=bin/boot_sector.bin bs=512 count=1
 start utils/dd.exe if=bin/boot.bin of=disk/boot.bin bs=1 skip=512
