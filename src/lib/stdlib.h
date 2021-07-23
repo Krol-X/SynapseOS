1b8b5fc94536424e2ec303b90dfd897361e086a8
0Nera <71536443+0Nera@users.noreply.github.com>
2021-07-23 15:45:02 +0300

    Add new functions

--- src/lib/stdlib.h
+++ src/lib/stdlib.h
@@ -29,6 +29,15 @@ typedef signed long long int64;
 #define min(a, b) (((a) > (b)) ? (b) : (a))
 #define max(a, b) (((a) > (b)) ? (a) : (b))
 
+
+#define outportb(port, value) asm("outb %b0, %w1"::"a"(value),"d"(port));
+#define outportw(port, value) asm("outw %w0, %w1"::"a"(value),"d"(port));
+#define outportl(port, value) asm("outl %0, %w1"::"a"(value),"d"(port));
+#define inportb(port, out_value) asm("inb %w1, %b0":"=a"(value):"d"(port));
+#define inportw(port, out_value) asm("inw %w1, %w0":"=a"(value):"d"(port));
+#define inportl(port, out_value) asm("inl %w1, %0":"=a"(value):"d"(port));
+
+
 void memset(void *mem, char value, size_t count);
 void memset_word(void *mem, uint16 value, size_t count);
 void memcpy(void *dest, void *src, size_t count);
