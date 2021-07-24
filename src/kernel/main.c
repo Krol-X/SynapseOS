b1e27bc909b0140909699f39c0d978641858e474
0Nera <71536443+0Nera@users.noreply.github.com>
2021-07-24 13:00:56 +0300

    Add scancodes

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,6 +1,8 @@
 #include "../lib/stdlib.h"
-#include "../lib/tty.h"
 #include "../lib/interrupts.h"
+#include "../lib/tty.h"
+
+#define OS_VERSION "0002"
 
 typedef struct {
     uint64 base;
@@ -10,16 +12,24 @@ typedef struct {
 void kernel_main(uint8 boot_disk_id, void *memory_map, BootModuleInfo *boot_module_list) {
     init_interrupts();
     init_tty();
-    
-    set_text_attr(0x1F);
+    set_text_attr(11);
     clear_screen();
+    int i;
+    printf("***SynapseOS %s***\n\n", OS_VERSION);
 
-    out_string("***SynapseOS 0001***\n\n");
+    set_text_attr(7);
     printf("Boot disk id is %d\n", boot_disk_id);
     printf("Memory map at 0x%x\n", memory_map);
     printf("Boot module list at 0x%x\n", boot_module_list);
-    printf("String is %s, char is %c, number is %d, hex number is 0x%x\n\n",
-        __DATE__, 'A', 1234, 0x1234);
+    //printf("String is %s, char is %c, number is %d, hex number is 0x%x\n\n", __DATE__, 'A', 1234, 0x1234);
+    printf("DATE is %s\n\n", __DATE__);
     printf("Enter help to get information about commands\n");
-
+    char buffer[100];
+    //i = 1 / 0;
+    while (true) {
+        out_string("Enter string: ");
+        in_string(buffer, sizeof(buffer));
+        printf("You typed: %s\n", buffer);
+    }
+    printf("You typed: %s\n", buffer);
 }
