e54099f3ec818171919f4bad372ecda363423cab
0Nera <71536443+0Nera@users.noreply.github.com>
2021-07-24 18:39:05 +0300

    Add clean function

--- build.bat
+++ build.bat
@@ -6,8 +6,8 @@ echo *************************
 ::Build bootloader
 echo build bootloader
 
-echo boot.bin
-start utils/FASM.EXE src/boot/boot.asm bin/boot.bin
+echo boot
+start utils/FASM.EXE src/boot/boot.asm bin/boot
 
 
 
@@ -21,11 +21,6 @@ echo interrupts.o
 i686-elf-gcc -c -m32 -ffreestanding -o bin/interrupts.o src/lib/interrupts.c
 
 
-::Build scancodes
-echo scancodes.o
-i686-elf-gcc -c -m32 -ffreestanding -o bin/scancodes.o src/lib/scancodes.c
-
-
 ::Build Stdlib
 echo stdlib.o
 i686-elf-gcc -c -m32 -ffreestanding -o bin/stdlib.o src/lib/stdlib.c
@@ -35,6 +30,10 @@ i686-elf-gcc -c -m32 -ffreestanding -o bin/stdlib.o src/lib/stdlib.c
 echo tty.o
 i686-elf-gcc -c -m32 -ffreestanding -o bin/tty.o src/lib/tty.c
 
+
+::Build scancodes
+echo scancodes.o
+i686-elf-gcc -c -m32 -ffreestanding -o bin/scancodes.o src/lib/scancodes.c
 
 
 echo *************************
@@ -51,17 +50,24 @@ i686-elf-gcc -c -m32 -ffreestanding -o bin/main.o src/kernel/main.c
 
 ::Link kernel with starter and libs
 echo link 
-i686-elf-ld -T src/script.ld -o bin/kernel.bin bin/startup.o bin/stdlib.o bin/tty.o bin/scancodes.o bin/interrupts.o bin/main.o --entry _start
+i686-elf-ld -T src/script.ld -o bin/kernel bin/startup.o bin/stdlib.o bin/tty.o  bin/interrupts.o bin/scancodes.o bin/main.o --entry _start
 
 
 
 echo *************************
 ::Make img file
 echo Make disk.img
-start utils/dd.exe if=bin/boot.bin of=bin/boot_sector.bin bs=512 count=1
-start utils/dd.exe if=bin/boot.bin of=disk/boot.bin bs=1 skip=512
-cp bin/kernel.bin disk/kernel.bin
-start utils/make_listfs.exe of=disk.img bs=512 size=2880 boot=bin/boot_sector.bin src=./disk 
+start utils/dd.exe if=bin/boot of=bin/boot_sector bs=512 count=1
+start utils/dd.exe if=bin/boot of=disk/boot bs=1 skip=512
+cp bin/kernel disk/kernel.bin
+start utils/make_listfs.exe of=disk.img bs=512 size=2880 boot=bin/boot_sector src=./disk 
+
+
+
+
+echo *************************
+::Clean trash
+
 
 echo *************************
 echo Done
