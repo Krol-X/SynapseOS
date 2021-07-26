fffb1ed373e39a2de1f81bb40e8ed703aedf8e20
0Nera <71536443+0Nera@users.noreply.github.com>
2021-07-26 13:04:38 +0300

    Create build script

--- build.bat
+++ build.bat
@@ -1,74 +1,26 @@
 @Echo off
 
-echo build SynapseOS 0002
+echo build SynapseOS 0003
 echo *************************
 
-::Build bootloader
-echo build bootloader
-
-echo boot
-start utils/FASM.EXE src/boot/boot.asm bin/boot
-
-
-
-echo *************************
-::Build libs
-echo build libs
+SET AS=i686-elf-as
+SET CC=i686-elf-gcc
+SET LD=i686-elf-ld
+SET SRC=./src/kernel
+SET BLASTER=A220 I5 D1 P330
+ECHO %BLASTER%
 
 
-::Build Interrupts
-echo interrupts.o
-i686-elf-gcc -c -m32 -ffreestanding -o bin/interrupts.o src/lib/interrupts.c
+SET CCFLAGS = "-std=gnu99 -ffreestanding -Wall -Wextra"
+SET LDFLAGS = "-ffreestanding -nostdlib -lgcc"
 
+mkdir bin
 
-::Build Stdlib
-echo stdlib.o
-i686-elf-gcc -c -m32 -ffreestanding -o bin/stdlib.o src/lib/stdlib.c
+echo Build asm kernel
+fasm %SRC%/kernel.asm bin/kasm.o
 
+echo %CC% %CCFLAGS% -c %SRC%/kernel.c -o ./bin/kc.o
+start %CC% %CCFLAGS% -c %SRC%/kernel.c -o ./bin/kc.o
 
-::Build Text Driver
-echo tty.o
-i686-elf-gcc -c -m32 -ffreestanding -o bin/tty.o src/lib/tty.c
-
-
-::Build scancodes
-echo scancodes.o
-i686-elf-gcc -c -m32 -ffreestanding -o bin/scancodes.o src/lib/scancodes.c
-
-
-echo *************************
-::Build kernel
-echo build Kernel
-
-::Build Kernel starter
-echo startup.o
-start utils/FASM.EXE src/kernel/startup.asm bin/startup.o
-
-::Build Kernel
-echo main.o
-i686-elf-gcc -c -m32 -ffreestanding -o bin/main.o src/kernel/main.c
-
-::Link kernel with starter and libs
-echo link 
-i686-elf-ld -T src/script.ld -o bin/kernel bin/startup.o bin/stdlib.o bin/tty.o  bin/interrupts.o bin/scancodes.o bin/main.o --entry _start
-
-
-
-echo *************************
-::Make img file
-echo Make disk.img
-start utils/dd.exe if=bin/boot of=bin/boot_sector bs=512 count=1
-start utils/dd.exe if=bin/boot of=disk/boot bs=1 skip=512
-cp bin/kernel disk/kernel.bin
-start utils/make_listfs.exe of=disk.img bs=512 size=2880 boot=bin/boot_sector src=./disk 
-
-
-
-
-echo *************************
-::Clean trash
-
-
-echo *************************
 echo Done
 pause
