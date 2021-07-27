81c49e44402c5a5b2023e15b41f43abcac9ee59a
0Nera <71536443+0Nera@users.noreply.github.com>
2021-07-27 18:05:03 +0300

    Update kernel.asm

--- src/kernel/kernel.asm
+++ src/kernel/kernel.asm
@@ -10,36 +10,25 @@ section '.text' executable
 
 public start
 public keyboard_handler
-public read_port
-public write_port
 public load_idt
 public gdt_flush
 
-extrn kmain 		;this is defined in the c file
+extrn kmain             ;this is defined in the c file
 extrn keyboard_handler_main
 
-read_port:
-	mov edx, [esp + 4]
-			;al is the lower 8 bits of eax
-	in al, dx	;dx is the lower 16 bits of edx
-	ret
-
-write_port:
-	mov   edx, [esp + 4]    
-	mov   al, [esp + 4 + 4]  
-	out   dx, al  
-	ret
-
 load_idt:
-	mov edx, [esp + 4]
-	lidt [edx]
-	sti 				;turn on interrupts
-	ret
+        mov edx, [esp + 4]
+        lidt [edx]
+        sti                             ;turn on interrupts
+        ret
 
-; TODO need pusha / popa, preserving DF
-keyboard_handler:                 
-	call    keyboard_handler_main
-	iretd
+
+keyboard_handler:   
+        pushad
+        cld
+        call    keyboard_handler_main
+        popad
+        iretd
 
 ; This will set up our new segment registers. We need to do
 ; something special in order to set CS. We do what is called a
@@ -60,10 +49,11 @@ gdt_flush:
     ret               ; Returns back to the C code!
 
 start:
-	cli 				;block interrupts
-	mov esp, stack_space
-	call kmain
-	hlt 				;halt the CPU
+        cli                             ;block interrupts
+        mov esp, stack_space
+        call kmain
+        hlt                             ;halt the CPU
 
 section '.bss' 
-stack_space rb 65535 ; 64 kib for stack
+rb 65535 ; 64 kib for stack
+stack_space: ; this is stack top, stack grows downwards
