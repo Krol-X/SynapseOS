65f6fd2670a42308aae5ea09d96f68860d0049f9
0Nera <71536443+0Nera@users.noreply.github.com>
2021-08-04 13:02:52 +0300

    Add CAPS

--- src/kernel/modules/kbd.c
+++ src/kernel/modules/kbd.c
@@ -4,12 +4,14 @@
 #include  "../include/ports.h"
 #include  "../include/tty.h"
 #include  "../include/shell.h"
+#include  "../include/qemu_log.h"
 
 char  string_mem[STRING_MEM_MAX];
 int string_mem_counter  =  0;
+int SHIFT = 0;
+int CAPS = 0;
 
-unsigned  char keyboard_map[] =
-{
+unsigned  char keyboard_map[] = {
   0, 
   27, //`
   '1', '2',  '3', '4',  '5', '6',  '7', '8',	'9',  '0', 
@@ -21,14 +23,59 @@ unsigned  char keyboard_map[] =
   0,			/*  29 - Control */
   'a', 's',  'd', 'f',  'g', 'h',  'j', 'k',  'l', ';',	/*  39 */
   '\'',  '`', 
-  ' ',		/*  Left shift */
+  1,		/*  Left shift */
   '\\',  'z', 'x',  'c', 'v',  'b', 'n',			/*  49 */
   'm', ',',  '.', '/',  
-  0,				/*  Right  shift  */
+  1,				/*  Right  shift  */
   '*',
   0,	/*  Alt  */
   ' ',	/*  Space  bar  */
-  0,	/*  Caps lock  */
+  -1,	/*  Caps lock  */
+  0,	/*  59 - F1  key  ...  >  */
+  0, 0,  0, 0,  0, 0,  0, 0,
+  0,	/*  <  ...  F10  */
+  0,	/*  69 - Num lock*/
+  0,	/*  Scroll Lock  */
+  0,	/*  Home key */
+  0,	/*  Up Arrow */
+  0,	/*  Page Up  */
+  '-',
+  0,	/*  Left Arrow */
+  0,
+  0,	/*  Right  Arrow  */
+  '+',
+  0,	/*  79 - End key*/
+  0,	/*  Down Arrow */
+  0,	/*  Page Down  */
+  0,	/*  Insert Key */
+  0,	/*  Delete Key */
+  0, 0,  0,
+  0,	/*  F11  Key  */
+  0,	/*  F12  Key  */
+  0,	/*  All  other  keys are undefined */
+};
+
+unsigned  char keyboard_map_shifted[] =
+{
+  0, 
+  27, //`
+  '!', '@',  '#', '$',  '%', '^',  '&', '*',	'(',  ')', 
+  '_', '+',  
+  '\b',	/*  Backspace  */
+  '\t',			/*  Tab  */
+  'Q', 'W',  'E', 'R',	/*  19 */
+  'T', 'Y',  'U', 'I',  'O', 'P',  '{', '}',  '\n',	/*  Enter  key  */
+  0,			/*  29 - Control */
+  'A', 'S',  'D', 'F',  'G', 'H',  'J', 'K',  'L', ':',	/*  39 */
+  '"',  '`', 
+  1,		/*  Left shift */
+  '|',  'Z', 'X',  'C', 'V',  'B', 'N',			/*  49 */
+  'M', '<',  '>', '?',  
+  1,				/*  Right  shift  */
+  '*',
+  0,	/*  Alt  */
+  ' ',	/*  Space  bar  */
+  -1,	/*  Caps lock  */
   0,	/*  59 - F1  key  ...  >  */
   0, 0,  0, 0,  0, 0,  0, 0,
   0,	/*  <  ...  F10  */
@@ -59,6 +106,7 @@ void  kb_init(void)
 	outb(0x21 , 0xFD);
 }
 
+
 void  keyboard_handler_main(void)
 {
 	unsigned  char status;
@@ -71,35 +119,63 @@ void  keyboard_handler_main(void)
 	/*  Lowest bit of  status will  be set if  buffer is  not  empty  */
 	if  (status  &  0x01)  {
 		keycode = inb(KEYBOARD_DATA_PORT);
-		if(keycode  <  0)
+    qemu
+    if ( keycode  == 1 ) {
+			SHIFT = 1;
 			return;
+		}
+    if ( keycode  ==  -1 ) {
+      if ( CAPS == 0 ){
+        CAPS = 1;
+      } else {
+        CAPS = 0;
+      }
+    }
+		if(keycode  <  0){
+      return;
+    }
+
+    if (CAPS == 1)	{
+      if ( SHIFT == 0 ){
+        SHIFT = 1;
+      } else {
+        SHIFT = 0;
+      }
+      
+    }
 
 		if(keycode  == ENTER_KEY_CODE) {
 			tty_putchar('\n');
-   shell_exec(string_mem);
+      shell_exec(string_mem);
 
-   string_mem_counter  =  0;
-   memset(string_mem,  0, STRING_MEM_MAX);
+      string_mem_counter  =  0;
+      memset(string_mem,  0, STRING_MEM_MAX);
 
-   tty_setcolor(VGA_COLOR_LIGHT_GREEN);
-   tty_printf("\n>");
-   tty_setcolor(VGA_COLOR_LIGHT_CYAN);
+      tty_setcolor(VGA_COLOR_LIGHT_GREEN);
+      tty_printf("\n>");
+      tty_setcolor(VGA_COLOR_LIGHT_CYAN);
 			return;
 		}
-
-  tty_putchar(keyboard_map[(unsigned char) keycode]);
-  if (string_mem_counter!= STRING_MEM_MAX){
-    if (DEBUG==1){
-      tty_printf("\n%c\n",(unsigned char) keycode);
+    
+    if ( SHIFT == 0 ){
+      tty_putchar(keyboard_map[(unsigned char) keycode]);
+    } else {
+      tty_putchar(keyboard_map_shifted[(unsigned char) keycode]);
     }
-   string_mem[string_mem_counter]  =  keyboard_map[(unsigned char) keycode];
-   string_mem_counter++;
-  }  else{
-   tty_setcolor(VGA_COLOR_RED);
-   tty_printf("\nError:  Buffer is  full.  Buffer cleaned.\n");
-   string_mem_counter  =  0;
-   memset(string_mem,  0, STRING_MEM_MAX);
-  }
+    tty_putchar(keyboard_map[(unsigned char) keycode]);
+    if (string_mem_counter!= STRING_MEM_MAX){
+      if (DEBUG==1){
+        tty_printf("\n%c\n",(unsigned char) keycode);
+      }
+      
+    string_mem[string_mem_counter]  =  keyboard_map[(unsigned char) keycode];
+    string_mem_counter++;
+    }  else{
+    tty_setcolor(VGA_COLOR_RED);
+    tty_printf("\nError:  Buffer is  full.  Buffer cleaned.\n");
+    string_mem_counter  =  0;
+    memset(string_mem,  0, STRING_MEM_MAX);
+    }
 
   //vidptr[current_loc++]  =  keyboard_map[(unsigned char) keycode];
 		//vidptr[current_loc++] = 0x07;
