4362cbb928de03ddeb437b1b0439aa59ad44803b
0Nera <71536443+0Nera@users.noreply.github.com>
2021-08-04 15:05:54 +0300

    Rename log files

--- run_qemu.bat
+++ run_qemu.bat
@@ -1,3 +1,3 @@
 :: after -m written ram size in mb
-qemu-system-i386 -m 32 -cdrom SynapseOS.iso -monitor stdio -serial file:SynapseOS_log.txt -no-reboot 
+qemu-system-i386 -m 32 -cdrom SynapseOS.iso -monitor stdio -serial file:Qemu_log.txt -no-reboot 
 :: -d cpu_reset
