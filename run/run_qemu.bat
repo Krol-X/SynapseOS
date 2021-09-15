e7b67809c14945e955f9e1fb37df4e681df88eef
0Nera <71536443+0Nera@users.noreply.github.com>
2021-09-15 21:28:27 +0300

    fix run script

--- run/run_qemu.bat
+++ run/run_qemu.bat
@@ -1,6 +1,6 @@
 :: Qemu config
 
 ::qemu-system-x86_64 -m 32 -kernel bin/kernel.elf -monitor stdio -serial file:Qemu_log.txt -no-reboot 
-qemu-system-i386 -m 32 -boot d -cdrom file:../SynapseOS.iso -fda file:A.img -fdb file:B.img -monitor stdio -serial file:Qemu_log.txt -no-reboot 
+qemu-system-i386 -m 32 -boot d -cdrom file:../SynapseOS.iso -monitor stdio -serial file:Qemu_log.txt -no-reboot 
 
 pause
